64382ecf780af335c16d56dc40e7bcce64897d2c
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-05-24 01:54:41 +0100

    completed the update command

--- console.py
+++ console.py
@@ -10,21 +10,25 @@ fs = FileStorage()
 
 classes = {"BaseModel":BaseModel}
 
-cls_missing = '"** class name missing **"'
+cls_missing = "** class name missing **"
 cls_nonex = "** class doesn't exist **"
 id_missing = "** instance id missing **"
 inst_not_fnd = "** no instance found **"
 attr_missing = "** attribute name missing **"
+value_missing = "** value missing **"
 
                 
 class HBNBCommand(cmd.Cmd):
     """Simple command line interface for AirBnB clone project"""
     prompt = '(hbnb)'
 
+    ## ERROR MANAGEMENT ###
+
     def call_err_msg(self, msg):
         """prints error message"""
         self.nohelp = msg
         print(self.nohelp)
+
 
     def class_check(self, line):
         """checks and validates the class name in command line"""
@@ -38,6 +42,7 @@ class HBNBCommand(cmd.Cmd):
                     self.call_err_msg(cls_nonex)
                     return 0
                 return 1, args
+
 
     def id_check(self, line):
         """checks and validates the id arg in command line"""
@@ -58,21 +63,20 @@ class HBNBCommand(cmd.Cmd):
                 self.call_err_msg(id_missing)
                 return 0
 
-    '''def attr_check(self, line):
+
+    def attr_check(self, line):
         flag = self.id_check(line)
         if flag:
             args = flag[2]
             all_objs = flag[1]
-            if len(args) > 2:
-                try:
-                    for key, obj in all_objs.items():
-                        value = getattr(obj, args[2])
-                        return 1, all_objs, args, value
-                except:
-                    self.call_err_msg(attr_missing)
-                    return 0
-            elif len(args) > 3:'''
-
+            if len(args) == 2:
+                self.call_err_msg(attr_missing)
+                return 0
+            elif len(args) == 3:
+                self.call_err_msg(value_missing)
+                return 0
+            else:
+                return 1
 
 
 
@@ -89,20 +93,20 @@ class HBNBCommand(cmd.Cmd):
     def do_show(self, line):
         """prints details of instance"""
         if self.id_check(line):
-            id_check_value = self.id_check(line)
-            args = id_check_value[2]
-            all_objs = id_check_value[1]
+            args = line.split()
+            all_objs = fs.all()
             print(all_objs[f"{args[0]}.{args[1]}"])
+
 
     def do_destroy(self, line):
         """deletes an instance from record"""   
         if self.id_check(line):
-            id_check_value = self.id_check(line)
-            args = id_check_value[2]
+            args = line.split()
             fs._FileStorage__objects.pop(f"{args[0]}.{args[1]}")
             fs.save()
             print(f"instance {args[1]} destroyed")
-    
+
+
     def do_all(self, line):
         """prints the string repr of all instances"""
         all_objs = fs.all()
@@ -110,22 +114,29 @@ class HBNBCommand(cmd.Cmd):
             print([str(obj) for obj in all_objs.values()])
         else:
             if self.class_check(line):
-                flag = self.class_check(line)
-                args = flag[1]
+                args = line.split()
                 obj_list = [str(all_objs[key]) for key in all_objs.keys() if key.startswith(args[0])]
                 print(obj_list)
 
-    def do_update(self, class_name, instance_id, attr, attr_val):
+
+    def do_update(self, line):
         """updates or adds attr to instance"""
-        pass
+        if self.attr_check(line):
+            args = line.split()
+            key = f"{args[0]}.{args[1]}"
+            setattr(fs._FileStorage__objects[key], args[2], args[3])
+            fs.save()
+
 
     def do_quit(self, arg):
         """Quit command: Exits the program"""
         sys.exit(1)
 
+
     def do_EOF(self, arg):
         """ EOF: quits program"""
         return True
+
 
     ###### THE HELPS #######
     def help_create(self):
